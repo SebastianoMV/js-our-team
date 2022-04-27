f631f9884cabd3ffe62e5387879c73495bcc6062
SebastianoMV <sebastianovisco@gmail.com>
2022-04-27 15:50:47 +0200

    Completamento con inserimento dinamico

--- script.js
+++ script.js
@@ -1,47 +1,69 @@
+const btnAdd = document.querySelector('#addMemberButton');
+
 const ourTeam = [
   {
     nome: 'Wayne Barnett',
     ruolo: 'Founder & CEO',
-    img: 'img/wayne-barnett-founder-ceo.jpg',
+    immagine: 'img/wayne-barnett-founder-ceo.jpg',
   },
   {
     nome: 'Angela Caroll',
     ruolo: 'Chief Editor',
-    img: 'img/angela-caroll-chief-editor.jpg',
+    immagine: 'img/angela-caroll-chief-editor.jpg',
   },
   {
     nome: 'Walter Gordon',
     ruolo: 'Office Manager',
-    img: 'img/walter-gordon-office-manager.jpg',
+    immagine: 'img/walter-gordon-office-manager.jpg',
   },
   {
     nome: 'Angela Lopez',
     ruolo: 'Social Media Manager',
-    img: 'img/angela-lopez-social-media-manager.jpg',
+    immagine: 'img/angela-lopez-social-media-manager.jpg',
   },
   {
     nome: 'Scott Estrada',
     ruolo: 'Developer',
-    img: 'img/scott-estrada-developer.jpg',
+    immagine: 'img/scott-estrada-developer.jpg',
   },
   {
     nome: 'Barbara Ramos',
     ruolo: 'Graphic Designer',
-    img: 'img/barbara-ramos-graphic-designer.jpg',
+    immagine: 'img/barbara-ramos-graphic-designer.jpg',
   },
 ];
 
+
+btnAdd.addEventListener('click', function(){
+  const newMember = {
+    nome: '',
+    ruolo:'',
+    immagine:''
+  };
+  newMember.nome = document.getElementById('name').value;
+  newMember.ruolo = document.getElementById('role').value;
+  newMember.immagine = document.getElementById('image').value;
+  ourTeam.push(newMember);
+  let i = ourTeam.length -1;
+  genera(i);
+});
+
+
+function genera(i){
+    const output = `
+    <div class="team-card">
+      <div class="card-image">
+        <img src="${ourTeam[i].immagine}"
+        alt="${ourTeam[i].nome}"/>
+      </div>
+      <div class="card-text">
+       <h3>${ourTeam[i].nome}</h3>
+       <p>${ourTeam[i].ruolo}</p>
+      </div>
+    </div>`
+    document.querySelector('.team-container').innerHTML += output;
+}
+
 for(let i in ourTeam){
-  const output = `
-  <div class="team-card">
-    <div class="card-image">
-      <img src="${ourTeam[i].img}"
-      alt="Wayne Barnett"/>
-    </div>
-    <div class="card-text">
-     <h3>${ourTeam[i].nome}</h3>
-     <p>${ourTeam[i].ruolo}</p>
-    </div>
-  </div>`
-  document.querySelector('.team-container').innerHTML += output;
+  genera(i);
 }
